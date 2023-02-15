ae352096889a83fd5b1a7904966949e48a7c2442
AbhinavDhakal <adhakalsir@gmail.com>
2023-02-15 17:06:31 +0545

    almost

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import "./App.css";
-import Todo from "./Todo/Todo.js";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [todos, setTodos] = useState(getFromLocal()?.todos || []);
+  const [id, setId] = useState(getFromLocal()?.counter || 0);
+  const inputRef = useRef(null);
+
+  function saveToLocal(obj) {
+    localStorage.setItem("todos", JSON.stringify(obj));
+  }
+
+  saveToLocal({ counter: id, todos: todos });
+
+  function getFromLocal() {
+    return JSON.parse(localStorage.getItem("todos"));
+  }
 
   return (
     <div className="App">
-      <Todo />
-      <div id="counter">
-        <button
-          onClick={() => {
-            setCounter((prevCounter) => prevCounter + 1);
-          }}
-        >
-          +
+      <h1>Todo</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setTodos([
+            ...todos,
+            { id: id, task: inputRef.current.value, completed: false },
+          ]);
+          setId(id + 1);
+          inputRef.current.value = "";
+        }}
+      >
+        <input key="sex" ref={inputRef} type="text"></input>
+        <button key="red" type="submit">
+          Add
         </button>
-        <h2>{counter}</h2>
-        <button
-          onClick={() => {
-            setCounter((prevCounter) => prevCounter - 1);
-          }}
-        >
-          -
-        </button>
-      </div>
+      </form>
+
+      {todos.map((todo) => {
+        return (
+          <li key={todo.id}>
+            {todo.task}
+            <button>X</button>
+          </li>
+        );
+      })}
     </div>
   );
 };
--- src/Todo/Todo.js
+++
@@ -1,5 +0,0 @@
-const Todo = () => {
-  return <h1>Counter</h1>;
-};
-
-export default Todo;
